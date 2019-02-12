8c90b79d50eddbfa7b73dbddc8a4ae6cb20b75a3
joseferrer1090 <jcfb90@gmail.com>
2019-02-12 15:20:53 -0500

    Change => adding structure for route and components

---
+++ src/views/Correspondence/Outbox/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>Estoy probando las rutas del proyecto</p>
+      </div>
+    );
+  }
+}
+
+index.propTypes = {};
+
+export default index;
