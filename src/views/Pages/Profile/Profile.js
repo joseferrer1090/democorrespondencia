1219c1fbd2c049526637cfb4c76d02a2ea190395
joseferrer1090 <jcfb90@gmail.com>
2019-03-20 15:02:49 -0500

    Change => Adding structure profile and component

--- src/views/Pages/Profile/Profile.js
+++ src/views/Pages/Profile/Profile.js
@@ -1,5 +1,21 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  CardTitle,
+  CardText,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Row,
+  Col
+} from "reactstrap";
+import imgProfile from "./../../../assets/img/user_profile.svg";
 
 class Profile extends Component {
   constructor(props) {
@@ -8,8 +24,67 @@ class Profile extends Component {
   }
   render() {
     return (
-      <div>
-        <p>Apenas probando la interfaz y la ruta</p>
+      <div className="animated fadeIn">
+        <Row>
+          <Col sm="3">
+            <div className="card">
+              {" "}
+              <a
+                className="text-center"
+                onClick={() => {
+                  alert("Probando");
+                }}
+              >
+                <img
+                  className="img-responsive"
+                  src={imgProfile}
+                  style={{ margin: "10px" }}
+                />
+              </a>
+              <CardTitle>
+                <p className="text-center">
+                  {" "}
+                  Nombre del usuario{" "}
+                  <small className="form-text"> Administrador </small>{" "}
+                </p>
+                <address>
+                  <div style={{ margin: "10px ", fontSize: "13px" }}>
+                    {" "}
+                    <p className="text-center">
+                      <i className="fa fa-phone-square" />
+                      {"   "}+(1234) - 5678910
+                    </p>
+                    <p className="text-center">
+                      <i className="fa fa-envelope" /> {"   "} admin@admin.com
+                    </p>
+                  </div>
+                </address>
+              </CardTitle>
+            </div>
+            <div>
+              <Card>
+                <CardHeader>
+                  <i className="fa fa-lock" /> Permisos asignados
+                </CardHeader>
+                <CardBody>
+                  <p>Probando</p>
+                </CardBody>
+              </Card>
+            </div>
+          </Col>
+          <Col sm="9">
+            <div>
+              <Card body style={{ height: "420px" }}>
+                <p>Probando apenas</p>
+              </Card>
+            </div>
+            <div>
+              <Card body style={{ height: "120px" }}>
+                <p>Cambio de contraseña</p>
+              </Card>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
