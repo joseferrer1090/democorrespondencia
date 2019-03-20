fa4b4586b553e7ebccbb8df6db9c748850fe600e
joseferrer1090 <jcfb90@gmail.com>
2019-03-20 10:06:23 -0500

    change => Order menu for module correspondencia

--- src/_nav.js
+++ src/_nav.js
@@ -11,43 +11,65 @@ export default {
       icon: "cui-envelope-closed",
       children: [
         {
-          name: " Externa",
+          name: "Recibida",
           url: "/correspondence/external",
-          icon: "icon-cloud-upload"
+          icon: "cui-inbox"
+        },
+        {
+          name: "Despachada",
+          url: "/correspondencia/externa/despachada",
+          icon: "cui-share"
         },
         {
           name: "Interna",
           url: "/correspondence/internal",
-          icon: "icon-cloud-download"
+          icon: "icon-drawer",
+          children: [
+            {
+              name: "Entrantes",
+              url: "/correspondencia/interna/entrada",
+              icon: "cui-inbox"
+            },
+            {
+              name: "Salientes",
+              url: "/correspondencia/interna/salida",
+              icon: "cui-share"
+            },
+            {
+              name: "Crear nueva",
+              url: "/correspondencia/interna/nueva",
+              icon: "icon-pencil"
+            }
+          ]
         }
       ]
-    },
-    {
-      name: "Radicación",
-      url: "/radication",
-      icon: "cui-envelope-letter",
-      children: [
-        {
-          name: "radicación simple",
-          url: "/radication/simple",
-          icon: "cui-info"
-        },
-        {
-          name: "radicación de tramite",
-          url: "/radication/procedure",
-          icon: "icon-list"
-        }
-      ]
-    },
-    {
-      name: "Generar Edok",
-      url: "/edok",
-      icon: "cui-file"
-    },
-    {
-      name: "Remitente",
-      url: "/senders",
-      icon: "cui-people"
     }
+    // {
+    //   name: "Radicación",
+    //   url: "/radication",
+    //   icon: "cui-envelope-letter",
+    //   children: [
+    //     {
+    //       name: "radicación simple",
+    //       url: "/radication/simple",
+    //       icon: "cui-info"
+    //     },
+    //     {
+    //       name: "radicación de tramite",
+    //       url: "/radication/procedure",
+    //       icon: "icon-list"
+    //     }
+    //   ]
+    // },
+    // {
+    //   name: "Generar Edok",
+    //   url: "/edok",
+    //   icon: "cui-file"
+    // },
+    // {
+    //   name: "Remitente",
+    //   url: "/senders",
+    //   icon: "cui-people"
+    // }
   ]
 };
