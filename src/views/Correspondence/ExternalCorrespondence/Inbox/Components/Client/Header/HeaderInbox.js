efa83dc3573f696243e841f1fa911fb5c95ac395
joseferrer1090 <jcfb90@gmail.com>
2019-03-26 15:07:03 -0500

    Change => Adding Structure for principal menu inbox correspondence

--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Header/HeaderInbox.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Header/HeaderInbox.js
@@ -7,10 +7,77 @@ class Headerinbox extends Component {
     super(props);
     this.state = {};
   }
+
+  getItems = () => {
+    return [
+      {
+        key: "newItem",
+        name: "New",
+        cacheKey: "myCacheKey", // changing this key will invalidate this items cache
+        iconProps: {
+          iconName: "Add"
+        },
+        ariaLabel: "New",
+        subMenuProps: {
+          items: [
+            {
+              key: "emailMessage",
+              name: "Email message",
+              iconProps: {
+                iconName: "Mail"
+              },
+              "data-automation-id": "newEmailButton"
+            },
+            {
+              key: "calendarEvent",
+              name: "Calendar event",
+              iconProps: {
+                iconName: "Calendar"
+              }
+            }
+          ]
+        }
+      },
+      {
+        key: "upload",
+        name: "Upload",
+        iconProps: {
+          iconName: "Upload"
+        },
+        href: "https://dev.office.com/fabric",
+        "data-automation-id": "uploadButton"
+      },
+      {
+        key: "share",
+        name: "Share",
+        iconProps: {
+          iconName: "Share"
+        },
+        onClick: () => console.log("Share")
+      },
+      {
+        key: "download",
+        name: "Download",
+        iconProps: {
+          iconName: "Download"
+        },
+        onClick: () => console.log("Download")
+      }
+    ];
+  };
+
   render() {
     return (
       <div className="col-md-12" style={{ border: "1px solid black" }}>
-        <p>Component header</p>
+        <CommandBar
+          items={this.getItems()}
+          //   overflowItems={this.getOverlflowItems()}
+          //   overflowButtonProps={{ ariaLabel: "More commands" }}
+          //   farItems={this.getFarItems()}
+          //   ariaLabel={
+          //     "Use left and right arrow keys to navigate between commands"
+          //   }
+        />
       </div>
     );
   }
