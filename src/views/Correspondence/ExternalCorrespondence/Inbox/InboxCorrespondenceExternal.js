47781094fcaa7c0c7c5976e69a4b47b0ddcf0350
jose carlos <jcfb90@gmail.com>
2019-03-26 00:48:24 -0500

    change => Adding strcuture for inbox correspondence

--- src/views/Correspondence/ExternalCorrespondence/Inbox/InboxCorrespondenceExternal.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/InboxCorrespondenceExternal.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
 import PropTypes from "prop-types";
 //import "./../../../../assets/css/interndashboard.css";
 
@@ -10,8 +11,92 @@ class InboxCorrespondenceExternal extends Component {
 
   render() {
     return (
-      <div className="animated fadeIn">
-        <p>La vista de bandeja de correspondencia externa</p>
+      <div
+        className="row"
+        style={{ marginTop: "-25px", padding: "0px, 0px, 0px, 0px" }}
+      >
+        <div className="col-md-12">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">
+                    Home <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                    <div className="dropdown-divider" />
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </div>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">
+                    Disabled
+                  </a>
+                </li>
+              </ul>
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button
+                  className="btn btn-outline-success my-2 my-sm-0"
+                  type="submit"
+                >
+                  Search
+                </button>
+              </form>
+            </div>
+          </nav>
+        </div>
       </div>
     );
   }
