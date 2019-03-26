7e76a72cea1f40dcdfd7a88ad8f22f888b60a3ab
joseferrer1090 <jcfb90@gmail.com>
2019-03-26 16:15:48 -0500

    Change => Adding othe component for menu left

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { initializeIcons } from "@uifabric/icons";
+
 initializeIcons();
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Sidebar/SidebarInboxComponent.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Sidebar/SidebarInboxComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Nav, INavLink } from "office-ui-fabric-react";
 import PropTypes from "prop-types";
 
 class SidebarInboxComponent extends Component {
@@ -8,11 +9,66 @@ class SidebarInboxComponent extends Component {
   }
   render() {
     return (
-      <div
-        className="col-md-2"
-        style={{ border: "2px solid red", height: "540px " }}
-      >
-        <p>Sidebar Component</p>
+      <div className="col-md-2" style={{ height: "540px " }}>
+        <div className="" style={{ width: "108%" }}>
+          <Nav
+            groups={[
+              {
+                links: [
+                  {
+                    name: "Home",
+                    url: "http://example.com",
+                    links: [
+                      {
+                        name: "Activity",
+                        url: "http://msn.com",
+                        key: "key1"
+                      },
+                      {
+                        name: "MSN",
+                        url: "http://msn.com",
+                        key: "key2"
+                      }
+                    ],
+                    isExpanded: true
+                  },
+                  {
+                    name: "Documents",
+                    url: "http://example.com",
+                    key: "key3",
+                    isExpanded: true
+                  },
+                  {
+                    name: "Pages",
+                    url: "http://msn.com",
+                    key: "key4"
+                  },
+                  {
+                    name: "Notebook",
+                    url: "http://msn.com",
+                    key: "key5"
+                  },
+                  {
+                    name: "Communication and Media",
+                    url: "http://msn.com",
+                    key: "key6"
+                  },
+                  {
+                    name: "News",
+                    url: "http://cnn.com",
+                    icon: "News",
+                    key: "key7"
+                  }
+                ]
+              }
+            ]}
+            onLinkClick={this.onLinkClick}
+            expandedStateText={"expanded"}
+            collapsedStateText={"collapsed"}
+            selectedKey={"key3"}
+            expandButtonAriaLabel={"Expand or collapse"}
+          />
+        </div>
       </div>
     );
   }
