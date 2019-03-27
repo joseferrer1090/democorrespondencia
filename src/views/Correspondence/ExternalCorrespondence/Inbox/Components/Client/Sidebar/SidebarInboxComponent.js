95d17d76e381dcc1224e86851d3f605a783da3b9
joseferrer1090 <jcfb90@gmail.com>
2019-03-27 12:11:17 -0500

    Change => Adding structure for sidebar menu

--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Sidebar/SidebarInboxComponent.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Sidebar/SidebarInboxComponent.js
@@ -16,70 +16,28 @@ class SidebarInboxComponent extends Component {
               {
                 links: [
                   {
-                    name: "Home",
-                    url: "http://example.com",
+                    name: "Correspondencia",
+                    url: "",
                     links: [
                       {
-                        name: "Activity",
-                        url: "http://msn.com",
+                        name: "Recibida",
+                        url:
+                          "http://localhost:3001/#/correspondence/external/recibida",
                         key: "key1"
                       },
                       {
-                        name: "MSN",
-                        url: "http://msn.com",
+                        name: "Despachada",
+                        url:
+                          "http://localhost:3001/#/correspondence/external/recibida",
                         key: "key2"
                       }
                     ],
                     isExpanded: true
                   },
                   {
-                    name: "Documents",
+                    name: "Consultar",
                     url: "http://example.com",
                     key: "key3",
-                    isExpanded: true
-                  },
-                  {
-                    name: "Pages",
-                    url: "http://msn.com",
-                    key: "key4"
-                  },
-                  {
-                    name: "Notebook",
-                    url: "http://msn.com",
-                    key: "key5"
-                  },
-                  {
-                    name: "Communication and Media",
-                    url: "http://msn.com",
-                    key: "key6"
-                  },
-                  {
-                    name: "News",
-                    url: "http://cnn.com",
-                    icon: "News",
-                    key: "key7"
-                  },
-                  {
-                    name: "Other",
-                    url: "http://example.com",
-                    links: [
-                      {
-                        name: "Activity",
-                        url: "http://msn.com",
-                        key: "key1"
-                      },
-                      {
-                        name: "MSN",
-                        url: "http://msn.com",
-                        key: "key2"
-                      },
-                      {
-                        name: "News",
-                        url: "http://cnn.com",
-                        icon: "News",
-                        key: "key7"
-                      }
-                    ],
                     isExpanded: true
                   }
                 ]
@@ -88,7 +46,6 @@ class SidebarInboxComponent extends Component {
             onLinkClick={this.onLinkClick}
             expandedStateText={"expanded"}
             collapsedStateText={"collapsed"}
-            selectedKey={"key3"}
             expandButtonAriaLabel={"Expand or collapse"}
           />
         </div>
