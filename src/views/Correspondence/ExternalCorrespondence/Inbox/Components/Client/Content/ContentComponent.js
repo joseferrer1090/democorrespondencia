139ac1efa7c7ab53f8e55c4e5d7ebb62de6065a4
joseferrer1090 <jcfb90@gmail.com>
2019-04-03 11:41:28 -0500

    Change => new structure for inbox correspondence internal component content build

--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/ContentComponent.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/ContentComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Data2 from "./../../../../../../../services/data";
+import "./components/css/table_inbox.css";
 
 class ContentComponent extends Component {
   constructor(props) {
@@ -16,13 +17,57 @@ class ContentComponent extends Component {
 
   render() {
     return (
-      <main
-        role="main"
-        className="col-md-10 col-lg-10 col-md-10"
-        style={{ border: "1px solid green", padding: "0px" }}
-      >
-        <p>Probando apenas</p>
-      </main>
+      <div className="animated fadeIn">
+        <table className="table table-inbox table-hover">
+          <tbody>
+            <tr className="table-info">
+              <td className="inbox-small-cells">
+                <input type="checkbox" className="mail-checkbox" />
+              </td>
+              <td className="inbox-small-cells">
+                <i class="fa fa-star" />
+              </td>
+              <td className="view-message  dont-show">PHPClass</td>
+              <td className="view-message ">
+                Added a new class: Login Class Fast Site
+              </td>
+              <td className="view-message  inbox-small-cells">
+                <i className="fa fa-paperclip" />
+              </td>
+              <td className="view-message  text-right">9:27 AM</td>
+            </tr>
+            <tr className="table-danger">
+              <td className="inbox-small-cells">
+                <input type="checkbox" className="mail-checkbox" />
+              </td>
+              <td className="inbox-small-cells">
+                <i className="fa fa-star" />
+              </td>
+              <td className="view-message dont-show">Google Webmaster </td>
+              <td className="view-message">
+                Improve the search presence of WebSite
+              </td>
+              <td className="view-message inbox-small-cells" />
+              <td className="view-message text-right">March 15</td>
+            </tr>
+
+            <tr className="table-success">
+              <td className="inbox-small-cells">
+                <input type="checkbox" className="mail-checkbox" />
+              </td>
+              <td className="inbox-small-cells">
+                <i className="fa fa-star" />
+              </td>
+              <td className="view-message dont-show">Google Webmaster </td>
+              <td className="view-message">
+                Improve the search presence of WebSite
+              </td>
+              <td className="view-message inbox-small-cells" />
+              <td className="view-message text-right">March 15</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
--- src/views/Correspondence/ExternalCorrespondence/Inbox/InboxCorrespondenceExternal.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/InboxCorrespondenceExternal.js
@@ -29,30 +29,9 @@ class InboxCorrespondenceExternal extends Component {
                 <SidebarInbox />
                 <div
                   className="col-md-10"
-                  style={{ border: "1px solid black" }}
+                  style={{ border: "1px solid black", padding: "0px" }}
                 >
-                  <div className="jumbotron">
-                    <h1>Navbar example</h1>
-                    <p>
-                      This example is a quick exercise to illustrate how the
-                      default, static and fixed to top navbar work. It includes
-                      the responsive CSS and HTML, so it also adapts to your
-                      viewport and device.
-                    </p>
-                    <p>
-                      To see the difference between static and fixed top
-                      navbars, just scroll.
-                    </p>
-                    <p>
-                      <a
-                        className="btn btn-lg btn-primary"
-                        href="../../components/#navbar"
-                        role="button"
-                      >
-                        View navbar docs »
-                      </a>
-                    </p>
-                  </div>
+                  <ContentInbox />
                 </div>
               </div>
             </div>
