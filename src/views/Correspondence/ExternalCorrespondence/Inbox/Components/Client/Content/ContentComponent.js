31be8ac8a97076b784d493d5108ed5c69535f66b
joseferrer1090 <jcfb90@gmail.com>
2019-04-04 12:16:55 -0500

    Change => Final component inbox correspondence

--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/ContentComponent.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/ContentComponent.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import {
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 import Data2 from "./../../../../../../../services/data";
 import "./components/css/table_inbox.css";
 
@@ -7,9 +13,16 @@ class ContentComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: Data2.data
+      data: Data2.data,
+      dropdownOpen: false
     };
   }
+
+  toggle = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  };
 
   componentDidMount() {
     console.log("Probando el component did mount");
@@ -21,103 +34,57 @@ class ContentComponent extends Component {
         <div className="inbox-body">
           <div className="mail-option">
             <div className="chk-all" style={{ marginLeft: "-9px" }}>
-              <input
-                type="checkbox"
-                className="mail-checkbox mail-group-checkbox"
-              />
-              <div className="btn-group">
-                <a
-                  data-toggle="dropdown"
-                  href="#"
-                  className="btn mini all"
-                  aria-expanded="false"
-                >
-                  Seleccionar todo
-                  <i className="fa fa-angle-down " />
-                </a>
-              </div>
-            </div>
-            <div className="btn-group">
-              <a
-                data-original-title="Refresh"
-                data-placement="top"
-                data-toggle="dropdown"
-                href="#"
-                className="
-                
-                3
-                btn mini tooltips"
+              <ButtonDropdown
+                isOpen={this.state.dropdownOpen}
+                toggle={this.toggle}
               >
-                <i className=" fa fa-refresh" />
-              </a>
+                <DropdownToggle caret size="sm">
+                  Acciones
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>Seleccionar todo</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
             </div>
-
-            <div className="btn-group hidden-phone">
-              <a
-                data-toggle="dropdown"
-                href="#"
-                className="btn mini blue"
-                aria-expanded="false"
-              >
-                More
-                <i className="fa fa-angle-down " />
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a href="#">
-                    <i className="fa fa-pencil" /> Mark as Read
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-ban" /> Spam
-                  </a>
-                </li>
-                <li className="divider" />
-                <li>
-                  <a href="#">
-                    <i className="fa fa-trash-o" /> Delete
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="btn-group">
-              <a data-toggle="dropdown" href="#" className="btn mini blue">
-                Move to
-                <i className="fa fa-angle-down " />
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a href="#">
-                    <i className="fa fa-pencil" /> Mark as Read
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-ban" /> Spam
-                  </a>
-                </li>
-                <li className="divider" />
-                <li>
-                  <a href="#">
-                    <i className="fa fa-trash-o" /> Delete
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            {/* <div className="custom-control custom-checkbox custom-control-inline">
-              <input
-                type="checkbox"
-                className="custom-control-input"
-                id="customRadio"
-                name="example"
-                defaultValue="customEx"
-              />
-              <label className="custom-control-label" htmlFor="customRadio">
-                Seleccionar todo
-              </label>
+            {/* <div className="btn-group">
+              <button className="btn btn-secondary btn-sm">
+                <i className="fa fa-refresh" />
+              </button>
             </div> */}
+
+            {/* <div className="btn-group hidden-phone">
+              <button className="btn btn-secondary btn-sm">more</button>
+            </div> */}
+
+            <div className="float-right">
+              <ul className="pagination">
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    <i className="fa fa-angle-double-left" />
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    1
+                  </a>
+                </li>
+                <li className="page-item ">
+                  <a className="page-link" href="#">
+                    2
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    3
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    <i className="fa fa-angle-double-right" />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <table className="table table-inbox table-hover">
