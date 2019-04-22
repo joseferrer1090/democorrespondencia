4baf30c0a95aa2372217d8ca2bc3c3b7c4b31606
joseferrer1090 <jcfb90@gmail.com>
2019-04-22 10:46:59 -0500

    Change => Adding structure in user related

--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/OtherOption/RelatedUsersCorrespondence.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/OtherOption/RelatedUsersCorrespondence.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import HeaderInbox from "./../../../Header/HeaderInbox";
+import SideBarInbox from "./../../../Sidebar/SidebarInboxComponent";
+import { Card } from "reactstrap";
 
 class RelatedUsersCorrespondence extends Component {
   constructor(props) {
@@ -9,7 +12,24 @@ class RelatedUsersCorrespondence extends Component {
   render() {
     return (
       <div>
-        <p>Probando apenas</p>
+        <HeaderInbox />
+        <div className="col-md-12">
+          <div
+            style={{
+              minHeight: "600px",
+              marginTop: "0px"
+            }}
+          >
+            <div className="row">
+              <SideBarInbox />
+              <div className="col-md-10" style={{ padding: "0px " }}>
+                <Card body>
+                  <p>Probando</p>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
