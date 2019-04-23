3a74ef3ef7d73eb0c470d62a8835845354723b2a
joseferrer1090 <jcfb90@gmail.com>
2019-04-23 09:08:34 -0500

    Change => Adding strucutre for historial correspondence

--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/OtherOption/HistorialCorrespondence.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/OtherOption/HistorialCorrespondence.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import HeaderInbox from "./../../../Header/HeaderInbox";
+import SideBarInbox from "./../../../Sidebar/SidebarInboxComponent";
 
 class HistorialCorrespondence extends Component {
   constructor(props) {
@@ -9,7 +11,22 @@ class HistorialCorrespondence extends Component {
   render() {
     return (
       <div>
-        <p>Probando apenas</p>
+        <HeaderInbox />
+        <div className="row">
+          <div className="col-md-12">
+            <div className="p-3 mb-2 bg-secondary text-dark">
+              Aqui va el Timeline horizontal con la unfirmacion
+            </div>
+          </div>
+          <div className="col-md-6" style={{ border: "1px solid red" }}>
+            {" "}
+            <p>La estructura fija</p>{" "}
+          </div>
+          <div className="col-md-6" style={{ border: "1px solid blue" }}>
+            {" "}
+            <p>La estructura dinamica</p>{" "}
+          </div>
+        </div>
       </div>
     );
   }
