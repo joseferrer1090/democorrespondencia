af0232432b20da2985b0cb6e3acd51003f694c03
joseferrer1090 <jcfb90@gmail.com>
2019-05-07 18:01:53 -0500

    Change => Adding steps from

---
+++ src/views/Radication/RadicationDocuments/Components/Steps/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="text-center">
+      <h4>Radicacíon de documentos</h4>
+    </div>
+  );
+};
+
+export default Title;
--- src/views/Radication/RadicationDocuments/RadicationDocument.js
+++ src/views/Radication/RadicationDocuments/RadicationDocument.js
@@ -1,16 +1,67 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import HeaderComponent from "./Components/Header/HeaderInbox";
+import "bs-stepper/dist/css/bs-stepper.css";
+import Title from "./Components/Steps/Title";
+import Stepper from "bs-stepper";
 
 class RadicationDocument extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  componentDidMount() {
+    this.stepper = new Stepper(document.querySelector("#stepper1"), {
+      linear: false,
+      animation: true
+    });
+  }
+
   render() {
     return (
       <div>
         <HeaderComponent />
+        <div className="row">
+          <div className="col-md-12" style={{ border: "1px solid green" }}>
+            <Title />
+          </div>
+          <div className="col-md-12" style={{ border: "1px solid red" }}>
+            <div id="strepper1" className="bs-stepper">
+              {/* En esta seccion va el contenido de los pasos */}
+              <div className="bs-strepper-header">
+                <div className="step" data-target="#test-1-1">
+                  <button className="step-trigger">
+                    <span className="bs-stepper-circle">1</span>
+                    <span className="bs-stepper-label">Radicar documento</span>
+                  </button>
+                </div>
+                <div className="line" />
+                <div className="step" data-target="#test-1-2">
+                  <button className="step-trigger">
+                    <span className="bs-stepper-circle">2</span>
+                    <span className="bs-stepper-label">Sticker</span>
+                  </button>
+                </div>
+                <div className="line" />
+                <div className="step" data-target="#test-1-3">
+                  <button className="step-trigger">
+                    <span className="bs-stepper-circle">3</span>
+                    <span className="bs-stepper-label">Adjuntar documento</span>
+                  </button>
+                </div>
+                <div className="line" />
+                <div className="step" data-target="#test-1-4">
+                  <button className="step-trigger">
+                    <span className="bs-stepper-circle">4</span>
+                    <span className="bs-stepper-label">Visulizar</span>
+                  </button>
+                </div>
+              </div>
+              {/* Fin de la cabecera */}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
