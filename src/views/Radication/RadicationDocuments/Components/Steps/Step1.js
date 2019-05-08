091fce74617202dde4814b0a6c921dcd3b46a92b
joseferrer1090 <jcfb90@gmail.com>
2019-05-08 18:05:21 -0500

    change => Adding structure for radication step 1

---
+++ src/views/Radication/RadicationDocuments/Components/Steps/Step1.js
@@ -0,0 +1,289 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Step1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <div className="">
+          <div className="col-md-10 offset-1">
+            <form className="form">
+              <div className="card">
+                <div className="p-2 mb-1 bg-secondary text-dark">
+                  Información básica
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="row">
+                        <div className="col-md-2 offset-1 ">
+                          <div className="form-group text-center">
+                            <label className="">
+                              <strong> Fecha de radicacion</strong>{" "}
+                            </label>
+                            <dd className="">04/10/2018</dd>
+                          </div>
+                        </div>
+                        <div className="col-md-2">
+                          <div className="form-group text-center">
+                            <label>
+                              {" "}
+                              <strong>Hora de radicacion</strong>{" "}
+                            </label>
+                            <dd> 09:57 AM </dd>
+                          </div>
+                        </div>
+                        <div className="col-md-2 ">
+                          <div className="form-group text-center">
+                            <label>
+                              {" "}
+                              <strong>Sede</strong>{" "}
+                            </label>
+                            <dd> Sede 1 </dd>
+                          </div>
+                        </div>
+                        <div className="col-md-2  ">
+                          <div className="form-group text-center">
+                            <label>
+                              {" "}
+                              <strong>Vigenica</strong>{" "}
+                            </label>
+                            <dd> 2018 </dd>
+                          </div>
+                        </div>
+                        <div className="col-md-2">
+                          <div className="form-group text-center">
+                            <label>
+                              {" "}
+                              <strong>Usuario radicador</strong>{" "}
+                            </label>
+                            <dd> Carmen Rojas </dd>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <hr />
+                    </div>
+                    <div className="col-md-12">
+                      <div className="row">
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              Tipo de documento{" "}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <select className="form-control form-control-sm">
+                              <option>Seleccione...</option>
+                              <option>Cuenta de cobro</option>
+                              <option>Cuenta de leasing</option>
+                              <option>Factura administrativa</option>
+                              <option>Factura de PME</option>
+                              <option>Factura de cadena</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              Fecha del documento{" "}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="date"
+                              className="form-control form-control-sm"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              Conglomerado{" "}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <select className="form-control form-control-sm">
+                              <option>Seleccione...</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              Empresa <span className="text-danger">*</span>
+                            </label>
+                            <select className="form-control form-control-sm">
+                              <option>Seleccione...</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              Sede <span className="text-danger">*</span>
+                            </label>
+                            <select className="form-control form-control-sm">
+                              <option>Seleccione...</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              Tipo de envio{" "}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <select className="form-control form-control-sm">
+                              <option>Seleccione...</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-md-12">
+                          <div className="form-group">
+                            <label>
+                              Asunto <span className="text-danger">*</span>
+                            </label>
+                            <textarea className="form-control" />
+                          </div>
+                        </div>
+                        <div className="col-md-12">
+                          <div className="form-group">
+                            <label>
+                              Observaciones{" "}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <textarea className="form-control" />
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              {" "}
+                              Folios <span className="text-danger">*</span>{" "}
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control form-control-sm"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              {" "}
+                              Folios <span className="text-danger">*</span>{" "}
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control form-control-sm"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-4">
+                          <div className="form-group">
+                            <label>
+                              {" "}
+                              Folios <span className="text-danger">*</span>{" "}
+                            </label>
+                            <select className="form-control form-control-sm">
+                              <option>Seleccione...</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="p-2 mb-1 bg-secondary text-dark">
+                  Enviado por
+                </div>
+                <div className="col-md-12">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label>Conglomerado</label>
+                        <select className="form-control form-control-sm">
+                          <option>Seleccione...</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label>Empresa</label>
+                        <select className="form-control form-control-sm">
+                          <option>Seleccione...</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label>Sede</label>
+                        <select className="form-control form-control-sm">
+                          <option>Seleccione...</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label>Dependencia</label>
+                        <select className="form-control form-control-sm">
+                          <option>Seleccione...</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label>Buscar remitente</label>
+                        <select className="form-control form-control-sm">
+                          <option>Seleccione...</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label>Remitente que firma</label>
+                        <select className="form-control form-control-sm">
+                          <option>Seleccione...</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="p-2 mb-1 bg-secondary text-dark">
+                  Destinatarios externos
+                </div>
+                <div className="col-md-12">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="form-group">
+                        <label>
+                          {" "}
+                          Buscar destinatario externo{" "}
+                          <span className="text-danger">*</span>
+                        </label>
+                        <select className="form-control form-contro-sm">
+                          <option>Seleccione...</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Step1.propTypes = {};
+
+export default Step1;
--- src/views/Radication/RadicationDocuments/RadicationDocument.js
+++ src/views/Radication/RadicationDocuments/RadicationDocument.js
@@ -4,6 +4,7 @@ import HeaderComponent from "./Components/Header/HeaderInbox";
 import "bs-stepper/dist/css/bs-stepper.css";
 import Title from "./Components/Steps/Title";
 import Stepper from "bs-stepper";
+import Step1 from "./Components/Steps/Step1";
 
 class RadicationDocument extends Component {
   constructor(props) {
@@ -12,72 +13,101 @@ class RadicationDocument extends Component {
   }
 
   componentDidMount() {
-    this.stepper = new Stepper(document.querySelector('#stepper'), {
+    this.stepper = new Stepper(document.querySelector("#stepper"), {
       linear: false,
       animation: true
-    })
+    });
   }
 
-  onSubmit(e){
+  onSubmit(e) {
     e.preventDefault();
     alert("Probando apenas");
   }
 
-
   render() {
     return (
-      <div>
+      <div className="">
         <HeaderComponent />
-        <div className="row">
-          <div className="col-md-12">
-            <Title />
-          </div>
-          <div className="col-md-12">
-
-            <div id="stepper" className="bs-stepper">
-              <div className="bs-stepper-header">
-                <div className="step" data-target="#test-l-1">
-                  <button className="step-trigger">
-                    <span className="bs-stepper-circle">1</span>
-                    <span className="bs-stepper-label">Email</span>
-                  </button>
+        <div className="card card-body">
+          <div className="row">
+            <div className="col-md-12">
+              <Title />
+            </div>
+            <div className="col-md-12">
+              <div id="stepper" className="bs-stepper">
+                <div className="bs-stepper-header">
+                  <div className="step" data-target="#test-l-1">
+                    <button className="step-trigger">
+                      <span className="bs-stepper-circle">1</span>
+                      <span className="bs-stepper-label">
+                        Radicar documento
+                      </span>
+                    </button>
+                  </div>
+                  <div className="line" />
+                  <div className="step" data-target="#test-l-2">
+                    <button className="step-trigger">
+                      <span className="bs-stepper-circle">2</span>
+                      <span className="bs-stepper-label">Sticker</span>
+                    </button>
+                  </div>
+                  <div className="line" />
+                  <div className="step" data-target="#test-l-3">
+                    <button className="step-trigger">
+                      <span className="bs-stepper-circle">3</span>
+                      <span className="bs-stepper-label">
+                        Adjuntar documento
+                      </span>
+                    </button>
+                  </div>
+                  <div className="line" />
+                  <div className="step" data-target="#test-l-4">
+                    <button className="step-trigger">
+                      <span className="bs-stepper-circle">4</span>
+                      <span className="bs-stepper-label">Visualizar</span>
+                    </button>
+                  </div>
                 </div>
-                <div className="line"/>
-                <div className="step" data-target="#test-l-2">
-                  <button className="step-trigger">
-                    <span className="bs-stepper-circle">2</span>
-                    <span className="bs-stepper-label">Password</span>
-                  </button>
-                </div>
-                <div className="line"/>
-                <div className="step" data-target="#test-l-3">
-                  <button className="step-trigger">
-                    <span className="bs-stepper-circle">3</span>
-                    <span className="bs-stepper-label">Validate</span>
-                  </button>
-                </div>
-              </div>
-              <div className="bs-stepper-content">
-                <form onSubmit={this.onSubmit}>
-                  <div id="test-l-1" className="content">
-                    <div className="form-group">
-                      <label htmlFor="exampleInputEmail1">Email address</label>
-                      <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
+                <div className="bs-stepper-content">
+                  <form onSubmit={this.onSubmit}>
+                    <div id="test-l-1" className="content">
+                      <Step1 />
+                      <button
+                        type={"button"}
+                        className="btn btn-primary"
+                        onClick={() => this.stepper.next()}
+                      >
+                        &gt;Next
+                      </button>
                     </div>
-                    <button type={"button"} className="btn btn-primary" onClick={() => this.stepper.next()}>&gt;Next</button>
-                  </div>
-                  <div id="test-l-2" className="content">
-                    <div className="form-group">
-                      <label htmlFor="exampleInputPassword1">Password</label>
-                      <input type="password" className="form-control" id="exampleInputPassword1"
-                             placeholder="Password"/>
+                    <div id="test-l-2" className="content">
+                      <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="exampleInputPassword1"
+                          placeholder="Password"
+                        />
+                      </div>
+                      <button
+                        type={"button"}
+                        className="btn btn-primary"
+                        onClick={() => this.stepper.next()}
+                      >
+                        &gt;Next
+                      </button>
                     </div>
-                    <button type={"button"} className="btn btn-primary" onClick={() => this.stepper.next()}>&gt;Next</button>
-                  </div>
-                  <div id="test-l-3" className="content text-center">
-                    <button type="submit" className="btn btn-primary mt-5">Submit</button>
-                  </div>
-                </form>
+                    <div id="test-l-3" className="content text-center">
+                      <p>Probando apenas que pasa</p>
+                    </div>
+                    <div id="test-l-4" className="content text-center">
+                      <button type="submit" className="btn btn-primary mt-5">
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
