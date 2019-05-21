ae679414029f2ec32964f7e10bc4c43316521d93
joseferrer1090 <jcfb90@gmail.com>
2019-05-21 17:44:08 -0500

    Change => Adding react dropzone component

--- src/views/Radication/RadicationDocuments/RadicationInboxExternalDocument/Components/Step3.js
+++ src/views/Radication/RadicationDocuments/RadicationInboxExternalDocument/Components/Step3.js
@@ -1,22 +1,44 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "react";
 import { Card, CardBody, CardFooter, CardHeader } from "reactstrap";
+import DropzoneComponent from "react-dropzone-component";
+import "./../../../../../../node_modules/react-dropzone-component/styles/filepicker.css";
+import "./../../../../../../node_modules/dropzone/dist/min/dropzone.min.css";
+
+const eventHandlers = { addedfile: file => console.log(file) };
 
 class Step3 extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.djsConfig = {
+      addRemoveLinks: true,
+      acceptedFiles: "pdf"
+    };
+    this.componentConfig = {
+      iconFileTypes: ["pdf"],
+      showFileTypeIcon: true,
+      postUrl: ""
+    };
   }
   render() {
+    const config = this.componentConfig;
+    const djsConfig = this.djsConfig;
+
     return (
       <div className="animated fadeIn">
-        <div className="col-md-7 offset-3">
+        <div className="col-md-7 offset-2">
           <Card body>
             <div className="row">
               <div className="col-md-12">
                 <h4 className="text-muted text-center">
                   Por favor, adjunte el documento escaneado.{" "}
                 </h4>
+                <div className="row">
+                  <div className="col-md-12">
+                    <DropzoneComponent />
+                  </div>
+                </div>
               </div>
             </div>
           </Card>
