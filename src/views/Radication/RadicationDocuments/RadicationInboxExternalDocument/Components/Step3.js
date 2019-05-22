18c3eceb145478e44991090322e9f44c8a26efb3
joseferrer1090 <jcfb90@gmail.com>
2019-05-22 16:49:47 -0500

    Change => Adding in step3 input for file

--- src/views/Radication/RadicationDocuments/RadicationInboxExternalDocument/Components/Step3.js
+++ src/views/Radication/RadicationDocuments/RadicationInboxExternalDocument/Components/Step3.js
@@ -1,47 +1,103 @@
 import React, { Component, Fragment } from "react";
 import PropTypes from "react";
 import { Card, CardBody, CardFooter, CardHeader } from "reactstrap";
-import DropzoneComponent from "react-dropzone-component";
-import "./../../../../../../node_modules/react-dropzone-component/styles/filepicker.css";
-import "./../../../../../../node_modules/dropzone/dist/min/dropzone.min.css";
-
-const eventHandlers = { addedfile: file => console.log(file) };
+import Files from "react-files";
+import "./react-list.css";
 
 class Step3 extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.djsConfig = {
-      addRemoveLinks: true,
-      acceptedFiles: "pdf"
-    };
-    this.componentConfig = {
-      iconFileTypes: ["pdf"],
-      showFileTypeIcon: true,
-      postUrl: ""
+    this.state = {
+      files: []
     };
   }
-  render() {
-    const config = this.componentConfig;
-    const djsConfig = this.djsConfig;
 
+  onFilesChange = files => {
+    this.setState(
+      {
+        files
+      },
+      () => {
+        console.log(this.state.files);
+      }
+    );
+  };
+
+  onFilesError = (error, files) => {
+    console.log("error code" + error.code + ":" + error.message);
+  };
+
+  filesRemoveOne = file => {
+    this.refs.files.removeFile(file);
+  };
+
+  filesRemoveAll = () => {
+    this.refs.files.removeFiles();
+  };
+
+  render() {
     return (
       <div className="animated fadeIn">
         <div className="col-md-7 offset-2">
-          <Card body>
-            <div className="row">
-              <div className="col-md-12">
-                <h4 className="text-muted text-center">
-                  Por favor, adjunte el documento escaneado.{" "}
-                </h4>
-                <div className="row">
-                  <div className="col-md-12">
-                    <p>Probando</p>
+          <div className="card">
+            <div className="card-header"> Adjuntar documento </div>
+            <div className="card-body">
+              <Files
+                ref="files"
+                className="flies-dropzone-list"
+                style={{ height: "100px" }}
+                onChange={this.onFilesChange}
+                onError={this.onFilesError}
+                accepts={[".pdf"]}
+                maxFiles={1}
+                clickable
+              >
+                {this.state.files.length > 0 ? (
+                  <div className="files-list">
+                    <ul>
+                      {this.state.files.map(file => (
+                        <li className="files-list-item" key={file.id}>
+                          <div className="files-list-item-preview">
+                            {file.preview.type === "image" ? (
+                              <img
+                                className="files-list-item-preview-image"
+                                src={file.preview.url}
+                              />
+                            ) : (
+                              <div className="files-list-item-preview-extension">
+                                {file.extension}
+                              </div>
+                            )}
+                          </div>
+                          <div className="files-list-item-content">
+                            <div className="files-list-item-content-item files-list-item-content-item-1">
+                              {file.name}
+                            </div>
+                            <div className="files-list-item-content-item files-list-item-content-item-2">
+                              {file.sizeReadable}
+                            </div>
+                          </div>
+                          <div
+                            id={file.id}
+                            className="files-list-item-remove"
+                            onClick={this.filesRemoveOne.bind(this, file)} // eslint-disable-line
+                          />
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                </div>
-              </div>
+                ) : (
+                  <div className="row">
+                    <div className="col-md-12 mt-4 text-center">
+                      <h4 className="text-muted">
+                        Por favor, adjunte el documento escaneado.
+                      </h4>
+                    </div>
+                  </div>
+                )}
+              </Files>
             </div>
-          </Card>
+          </div>
         </div>
       </div>
     );
