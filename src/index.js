46ea0f13f7128b05cb1fbdcb07351d0642713f8c
jose carlos <jcfb90@gmail.com>
2020-06-22 00:56:21 -0500

    Cambios => empezando el empalme con el modulo de configuracion

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import Cookie from "js-cookie";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const dataauth = Cookie.get("auth");
 
+ReactDOM.render(
+  <App authorization={dataauth} />,
+  document.getElementById("root")
+);
+
+// ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
