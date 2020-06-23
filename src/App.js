69f273118823899429bdcc7806788d5653338991
jose carlos <jcfb90@gmail.com>
2020-06-23 01:36:06 -0500

    Cambios => Mejorando un poco la proteccion de las rutas

--- src/App.js
+++ src/App.js
@@ -5,65 +5,7 @@ import Cookie from "js-cookie";
 import Loadable from "react-loadable";
 import "./App.scss";
 import { decode } from "jsonwebtoken";
-
-const isAuthenticate = () => {
-  const auth = Cookie.get("auth");
-  const token = asyncLocalStorage.getItem("auth_token");
-  try {
-    if (token !== null && auth !== null) {
-      return true;
-    } else if (token === null && auth !== null) {
-      return false;
-    } else if (token === null && auth === null) {
-      return false;
-    }
-  } catch (error) {
-    console.log(`Error => ${error}`);
-    return false;
-  }
-};
-
-const VerifyToken = () => {
-  const auth = Cookie.get("auth");
-  const token = asyncLocalStorage.getItem("auth_token");
-
-  const aux = decode(auth);
-  const exp = aux.exp;
-  const now = new Date().getTime() / 100;
-
-  try {
-    if (now < exp && auth != null && token !== null) {
-      return true;
-    } else if (exp === null && now !== null) {
-      return false;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    console.log(`Error => ${error}`);
-    return false;
-  }
-};
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      try {
-        if (VerifyToken()) {
-          return <Component {...props} />;
-        } else if (isAuthenticate()) {
-          return <Component {...props} />;
-        } else {
-          return <Redirect path={{ pathname: "http://localhost:3000/#/" }} />;
-        }
-      } catch (err) {
-        console.log(`Error => ${err}`);
-        return <Redirect to={{ pathname: "/404" }} />;
-      }
-    }}
-  />
-);
+import { PrivateRoute } from "./utils/PrivateRoutes";
 
 const loading = () => (
   <div className="animated fadeIn pt-3 text-center">Loading...</div>
@@ -88,13 +30,56 @@ const Page500 = Loadable({
 });
 
 class App extends Component {
+  isAuthenticate = () => {
+    const auth = Cookie.get("auth");
+    const token = asyncLocalStorage.getItem("auth_token");
+    try {
+      if (token !== null && auth !== null) {
+        return true;
+      } else if (token === null && auth !== null) {
+        return false;
+      } else if (token === null && auth === null) {
+        return false;
+      }
+    } catch (error) {
+      console.log(`Error => ${error}`);
+      return false;
+    }
+  };
+  VerifyToken = () => {
+    const auth = Cookie.get("auth");
+    const token = asyncLocalStorage.getItem("auth_token");
+
+    const aux = decode(auth);
+    const exp = aux.exp;
+    const now = new Date().getTime() / 100;
+
+    try {
+      if (now < exp && auth != null && token !== null) {
+        return true;
+      } else if (exp === null && now !== null) {
+        return false;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(`Error => ${error}`);
+      return false;
+    }
+  };
   render() {
     return (
       <HashRouter>
         <Switch>
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />
-          <PrivateRoute path="/" name="Home" component={DefaultLayout} />
+          <PrivateRoute
+            path="/"
+            name="Home"
+            isAuthenticate={this.isAuthenticate}
+            verifyToken={this.VerifyToken}
+            component={DefaultLayout}
+          />
         </Switch>
       </HashRouter>
     );
