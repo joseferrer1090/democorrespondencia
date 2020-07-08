88099726aa9fedfe54b57d12ca9a535d29c290e6
joseferrer1090 <jcfb90@gmail.com>
2020-07-08 11:39:54 -0500

    Cambios => Maquetando la vista para la configuracion del sticker

--- src/views/Configuration/Sticker/Sticker.js
+++ src/views/Configuration/Sticker/Sticker.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import {
+  Card,
+  CardHeader,
+  ListGroup,
+  ListGroupItem,
+  CardBody,
+} from "reactstrap";
+import "./css/list.css";
 
 class Sticker extends Component {
   constructor(props) {
@@ -7,7 +14,60 @@ class Sticker extends Component {
     this.state = {};
   }
   render() {
-    return <div>Probanod</div>;
+    return (
+      <React.Fragment>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="alert alert-warning">
+              <i className="fa fa-exclamation-triangle" /> Los valores presente
+              son los posibles que va a estaran disponibles en el sticker de la
+              correspondencia
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header">
+                <input
+                  type="search"
+                  className="form-control form-control-sm"
+                  placeholder={`Buscar la propiedad asignar al sticker`}
+                />
+              </div>
+              <div className="card-body">
+                <ul className="list-group">
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                  <li className="list-group-item">probando</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <p>Drop</p>
+            <Card>
+              <CardBody>
+                <div className="row">
+                  <p>Los datos mas el orden</p>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12" style={{ border: "1px solid red" }}>
+            <p>Probando apenas</p>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 export default Sticker;
