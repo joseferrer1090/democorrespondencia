392bf5853377a5239bacd90fc9bc63042fd05342
jose carlos <jcfb90@gmail.com>
2020-07-07 22:04:42 -0500

    Cambios => Agregando las vista para configuracion del sticker

---
+++ src/views/Configuration/Sticker/EditSticker.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class EditSticker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Editar los valor del sticker</div>;
+  }
+}
+export default EditSticker;
---
+++ src/views/Configuration/Sticker/Sticker.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Sticker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Probanod</div>;
+  }
+}
+export default Sticker;
