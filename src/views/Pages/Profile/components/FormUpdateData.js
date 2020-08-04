beb578b3f8f360af4e755f56e5799bc57941b629
jose carlos <jcfb90@gmail.com>
2020-08-03 20:41:29 -0500

    Cambios => se agrego el onSubmit en el formulario de actualizacion

--- src/views/Pages/Profile/components/FormUpdateData.js
+++ src/views/Pages/Profile/components/FormUpdateData.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { Row, Col, Card, CardBody } from "reactstrap";
 import { getUser } from "./../../../../actions/authActions";
 import { decode } from "jsonwebtoken";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const FormUpdateData = () => {
   const user = decode(localStorage.getItem("auth_token"));
@@ -12,82 +14,104 @@ export const FormUpdateData = () => {
   };
   useEffect(() => {
     getDataUser();
+  }, []);
+  const formik = useFormik({
+    initialValues: {},
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
   });
   return (
-    <div className="animated fadeIn">
-      <Card>
-        <CardBody>
-          <div className="container">
-            <Row>
-              <Col sm="6">
-                <div className="form-group">
-                  <label> Identificación </label>
-                  <input
-                    type="text"
-                    className="form-control form-control-sm"
-                    disabled
-                    placeholder="identificación"
-                  />
-                </div>
-              </Col>
-              <Col sm="6">
-                <div className="form-group">
-                  <label> Nombre </label>
-                  <input type="text" className="form-control form-control-sm" />
-                </div>
-              </Col>
-              <Col sm="6">
-                <div className="form-group">
-                  <label> Fecha de nacimiento </label>
-                  <input type="text" className="form-control form-control-sm" />
-                </div>
-              </Col>
-              <Col sm="6">
-                <div className="form-group">
-                  <label> Telefono </label>
-                  <input type="text" className="form-control form-control-sm" />
-                </div>
-              </Col>
-              <Col sm="12">
-                <div className="form-group">
-                  <label> Direccion </label>
-                  <textarea className="form-control form-control-sm" />
-                </div>
-              </Col>
-              <Col sm="6">
-                <div className="form-group">
-                  <label> Email </label>
-                  <input
-                    type="text"
-                    className="form-control form-control-sm"
-                    disabled
-                    placeholder="email"
-                  />
-                </div>
-              </Col>
-              <Col sm="6">
-                <div className="form-group">
-                  <label> Usuario </label>
-                  <input
-                    type="text"
-                    className="form-control form-control-sm"
-                    disabled
-                    placeholder="usuario"
-                  />
-                </div>
-              </Col>
-              <Col sm="12">
-                <div className="float-right">
-                  <button type="button" className="btn btn-secondary btn-sm">
-                    {" "}
-                    <i className="fa fa-pencil" /> Actualizar perfil{" "}
-                  </button>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </CardBody>
-      </Card>
-    </div>
+    <React.Fragment>
+      <div className="animated fadeIn">
+        <form onSubmit={formik.handleSubmit}>
+          <Card>
+            <CardBody>
+              <div className="container">
+                <Row>
+                  <Col sm="6">
+                    <div className="form-group">
+                      <label> Identificación </label>
+                      <input
+                        type="text"
+                        className="form-control form-control-sm"
+                        disabled
+                        placeholder="identificación"
+                      />
+                    </div>
+                  </Col>
+                  <Col sm="6">
+                    <div className="form-group">
+                      <label> Nombre </label>
+                      <input
+                        type="text"
+                        className="form-control form-control-sm"
+                      />
+                    </div>
+                  </Col>
+                  <Col sm="6">
+                    <div className="form-group">
+                      <label> Fecha de nacimiento </label>
+                      <input
+                        type="text"
+                        className="form-control form-control-sm"
+                      />
+                    </div>
+                  </Col>
+                  <Col sm="6">
+                    <div className="form-group">
+                      <label> Telefono </label>
+                      <input
+                        type="text"
+                        className="form-control form-control-sm"
+                      />
+                    </div>
+                  </Col>
+                  <Col sm="12">
+                    <div className="form-group">
+                      <label> Direccion </label>
+                      <textarea className="form-control form-control-sm" />
+                    </div>
+                  </Col>
+                  <Col sm="6">
+                    <div className="form-group">
+                      <label> Email </label>
+                      <input
+                        type="text"
+                        className="form-control form-control-sm"
+                        disabled
+                        placeholder="email"
+                      />
+                    </div>
+                  </Col>
+                  <Col sm="6">
+                    <div className="form-group">
+                      <label> Usuario </label>
+                      <input
+                        type="text"
+                        className="form-control form-control-sm"
+                        disabled
+                        placeholder="usuario"
+                      />
+                    </div>
+                  </Col>
+                  <Col sm="12">
+                    <div className="float-right">
+                      <button
+                        type="submit"
+                        className="btn btn-secondary btn-sm"
+                      >
+                        {" "}
+                        <i className="fa fa-pencil" /> Actualizar perfil{" "}
+                      </button>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </CardBody>
+          </Card>
+        </form>
+      </div>
+    </React.Fragment>
   );
 };
