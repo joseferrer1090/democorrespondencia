64037ee36b388f3f5bfe9bd1b25552a9b6500eb1
jose carlos <jcfb90@gmail.com>
2020-08-04 00:13:34 -0500

    Cambios => se modificacion de perfil de usuario conectado

--- src/views/Pages/Profile/components/FormUpdateData.js
+++ src/views/Pages/Profile/components/FormUpdateData.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Row, Col, Card, CardBody } from "reactstrap";
+import { Row, Col, Card, CardBody, Alert } from "reactstrap";
 import { getUser } from "./../../../../actions/authActions";
 import { USER_PROFILE_UPDATE } from "./../../../../services/EndPoints";
 import { decode } from "jsonwebtoken";
@@ -11,20 +11,22 @@ import moment from "moment";
 export const FormUpdateData = () => {
   const token = localStorage.getItem("auth_token");
   const user = decode(localStorage.getItem("auth_token"));
+  const [alertSuccess, setAlertSuccess] = useState(false);
+  const [alertError, setAlertError] = useState(false);
+
   const dispatch = useDispatch();
   const getDataUser = () => {
     dispatch(getUser());
   };
-  const data = useSelector((state) => state.authReducer.user);
 
+  const data = useSelector((state) => state.authReducer.user);
+  const { id } = useSelector((state) => state.authReducer);
   useEffect(() => {
     getDataUser();
   }, []);
 
   const formatDate = (date) => {
-    let aux;
-    aux = new Date(date);
-    return moment(aux).format("DD-MM-YYYY");
+    return moment(date).format("YYYY-MM-DD");
   };
 
   const validationschema = () =>
@@ -41,13 +43,13 @@ export const FormUpdateData = () => {
     initialValues: {
       identification: data.identification || "",
       name: data.name || "",
-      birthdate: formatDate(data.birthDate) || "",
+      birthdate: data.birthDate || "",
       phone: data.phone || "",
       address: data.address || "",
       email: data.email || "",
       username: data.username || "",
     },
-    onSubmit: (values) => {
+    onSubmit: (values, { setSubmitting }) => {
       fetch(`${USER_PROFILE_UPDATE}`, {
         method: "PUT",
         headers: {
@@ -55,29 +57,40 @@ export const FormUpdateData = () => {
           Authorization: "Bearer " + token,
         },
         body: JSON.stringify({
+          id: id,
           userNameAuthenticate: user.user_name,
           name: values.name,
           phone: values.phone,
           address: values.address,
-          birthDate: formatDate(values.birthDate),
+          birthDate: formatDate(values.birthdate),
         }),
       })
         .then((response) => {
           if (response.ok) {
-            console.log(response);
+            setAlertSuccess(true);
+            setTimeout(() => {
+              setAlertSuccess(false);
+              setSubmitting(false);
+            }, 1200);
           } else if (response.status === 400) {
-            console.log("Validar los datos que se envian");
+            setAlertError(true);
+            setTimeout(() => {
+              setAlertError(false);
+              setSubmitting(false);
+            }, 1200);
           } else if (response.status === 500) {
-            console.log("Error al actualizar los datos");
+            setAlertError(true);
+            setTimeout(() => {
+              setAlertError(false);
+              setSubmitting(false);
+            }, 1200);
           }
         })
         .catch((err) => {
           console.log(`Error => ${err}`);
         });
-      // alert(JSON.stringify(values, null, 2));
     },
   });
-
   return (
     <React.Fragment>
       <div className="animated fadeIn">
@@ -85,6 +98,14 @@ export const FormUpdateData = () => {
           <Card>
             <CardBody>
               <div className="container">
+                <Alert color="success" isOpen={alertSuccess}>
+                  <i className="fa fa-check-circle" /> Se Actualizaron los datos
+                  de tu perfil
+                </Alert>
+                <Alert color="danger" isOpen={alertError}>
+                  <i className="fa fa-times" /> Error al Actualizar los datos de
+                  tu perfil
+                </Alert>
                 <Row>
                   <Col sm="6">
                     <div className="form-group">
@@ -93,7 +114,7 @@ export const FormUpdateData = () => {
                         name="identification"
                         id="identification"
                         type="text"
-                        className="form-control form-control-sm"
+                        className={`form-control form-control-sm`}
                         value={formik.values.identification}
                         onChange={formik.handleChange}
                         disabled
@@ -106,11 +127,21 @@ export const FormUpdateData = () => {
                       <input
                         name="name"
                         id="name"
+                        className={`form-control form-control-sm ${
+                          formik.errors.name &&
+                          formik.touched.name &&
+                          "is-invalid"
+                        }`}
                         value={formik.values.name}
                         onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         type="text"
-                        className="form-control form-control-sm"
                       />
+                      <div className="" style={{ color: "#D54B4B" }}>
+                        {formik.errors.name && formik.touched.name ? (
+                          <i className="fa fa-exclamation-triangle" />
+                        ) : null}
+                      </div>
                     </div>
                   </Col>
                   <Col sm="6">
@@ -119,9 +150,9 @@ export const FormUpdateData = () => {
                       <input
                         name="birthdate"
                         id="birthdate"
-                        type="text"
+                        type="date"
                         className="form-control form-control-sm"
-                        value={formik.values.birthdate}
+                        value={formatDate(formik.values.birthdate)}
                         onChange={formik.handleChange}
                       />
                     </div>
@@ -182,9 +213,15 @@ export const FormUpdateData = () => {
                       <button
                         type="submit"
                         className="btn btn-secondary btn-sm"
+                        disabled={formik.isSubmitting}
                       >
-                        {" "}
-                        <i className="fa fa-pencil" /> Actualizar perfil{" "}
+                        {formik.isSubmitting ? (
+                          <i className="fa fa-spinner fa-spin" />
+                        ) : (
+                          <div>
+                            <i className="fa fa-refresh" /> Actualizar perfil
+                          </div>
+                        )}
                       </button>
                     </div>
                   </Col>
