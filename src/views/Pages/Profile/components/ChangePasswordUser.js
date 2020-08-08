1fbc02d642c42c5f2df2343ec25403cbd5cb91a0
jose carlos <jcfb90@gmail.com>
2020-08-08 09:56:27 -0500

    Cambios => Se agrego formik con redux en el formulario para cambiar el password de user

--- src/views/Pages/Profile/components/ChangePasswordUser.js
+++ src/views/Pages/Profile/components/ChangePasswordUser.js
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { Formik, ErrorMessage, Form, withFormik } from "formik";
+import React from "react";
+import { withFormik } from "formik";
 import * as Yup from "yup";
+import { connect } from "react-redux";
 import {
   Row,
   Col,
@@ -12,21 +11,8 @@ import {
   Alert,
   CardHeader,
 } from "reactstrap";
-import { ChangePasswordAction } from "./../../../../actions/authActions";
 
-class ChangePasswordUser extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      alertSuccess: false,
-      alertError: false,
-    };
-  }
-
-  updatePassword = (username, oldpassword, newpassword) => {
-    this.props.changepassword(username, oldpassword, newpassword);
-  };
-
+class ChangePasswordUser extends React.Component {
   render() {
     const {
       values,
@@ -69,10 +55,12 @@ class ChangePasswordUser extends Component {
                             <span className="text-danger">*</span>{" "}
                           </label>
                           <input
-                            name="curent_password"
+                            name="oldpasswd"
                             className={`form-control form-control-sm `}
                             type="password"
-                            placeholder=""
+                            value={values.oldpasswd}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
                           />
                         </div>
                       </div>
@@ -84,10 +72,12 @@ class ChangePasswordUser extends Component {
                             <span className="text-danger">*</span>{" "}
                           </label>
                           <input
-                            name="new_password"
+                            name="newpasswd"
                             type="password"
                             className="form-control form-control-sm"
-                            placeholder={"Nueva contraseña"}
+                            value={values.newpasswd}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
                           />
                         </div>
                       </div>
@@ -99,10 +89,12 @@ class ChangePasswordUser extends Component {
                             <span className="text-danger">*</span>{" "}
                           </label>
                           <input
-                            name="confirm_password"
+                            name="confirmpasswd"
                             type="password"
                             className="form-control form-control-sm"
-                            placeholder={"Confirmar nueva contraseña"}
+                            value={values.confirmpasswd}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
                           />
                         </div>
                       </div>
@@ -111,7 +103,11 @@ class ChangePasswordUser extends Component {
                 </CardBody>
                 <CardFooter>
                   <div className="float-right">
-                    <button type="submit" className="btn btn-secondary btn-sm">
+                    <button
+                      type="submit"
+                      className="btn btn-secondary btn-sm"
+                      onClick={handleSubmit}
+                    >
                       <i className="fa fa-pencil" /> Actualizar contraseña
                     </button>
                   </div>
@@ -125,18 +121,17 @@ class ChangePasswordUser extends Component {
   }
 }
 
-ChangePasswordUser.propTypes = {};
+const formikEnhancer = withFormik({
+  validationSchema: Yup.object().shape({
+    oldpasswd: Yup.string().required("Necesario digitar la contraseña actual"),
+    newpasswd: Yup.string().required("No puede ir la contraseña vacia"),
+    confirmpasswd: Yup.string().required(
+      "Se Requiere confirmar la contraseña digita"
+    ),
+  }),
+  handleSubmit: (values, { props, setSubmitting }) => {
+    alert(JSON.stringify(values, 2, null));
+  },
+})(ChangePasswordUser);
 
-const mapStateToProps = (state) => {
-  return { state };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    changepassword: ({ username, oldpassword, newpassword }) => {
-      dispatch(ChangePasswordAction({ username, oldpassword, newpassword }));
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ChangePasswordUser);
+export default connect()(formikEnhancer);
