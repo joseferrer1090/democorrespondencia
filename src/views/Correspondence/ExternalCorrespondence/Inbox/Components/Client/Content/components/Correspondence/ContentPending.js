8ccf05a56df79bbea641c06201c8f8ce59fa61ae
jose carlos <jcfb90@gmail.com>
2020-09-22 12:01:13 -0500

    Cambios => base para los componente de correspondencia recibida y pendiente

---
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/Correspondence/ContentPending.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class ContentPending extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div>probando apenas</div>;
+  }
+}
+export default ContentPending;
---
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/Correspondence/ContentReceived.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class ContentReceived extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div>Probando el componente de recibidas</div>;
+  }
+}
+
+export default ContentReceived;
