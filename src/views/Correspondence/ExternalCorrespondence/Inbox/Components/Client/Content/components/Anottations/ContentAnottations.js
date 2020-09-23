094ca34ca20dd79d87b88b44024e22960511110f
jose carlos <jcfb90@gmail.com>
2020-09-23 17:05:23 -0500

    Cambios => Maquetado de anotaciones

--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/Anottations/ContentAnottations.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/Anottations/ContentAnottations.js
@@ -1,7 +1,21 @@
 import React, { Component, Fragment } from "react";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col,
+} from "reactstrap";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import InputSearch from "./../../InputSearch";
+import classnames from "classnames";
 
 /* Componente que va tener la informacion de las anotaciones y demas funcionalidades relacionadas con ella
  1- Maquetar el componete
@@ -16,21 +30,115 @@ import InputSearch from "./../../InputSearch";
 class ContentAnottations extends Component {
   constructor(props) {
     super();
+    this.state = {
+      activeTab: "1",
+    };
   }
+
+  toggle = (tab) => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab,
+      });
+    }
+  };
+
   render() {
     console.log(this.props);
     return (
       <Fragment>
         <div className="row">
-          <div className="col-md-7" style={{ border: "1px solid red" }}></div>
-          <div className="col-md-5" style={{ border: "1px solid blue" }}></div>
+          <div className="col-md-7">
+            <Nav tabs>
+              <NavItem>
+                <NavLink
+                  className={classnames({
+                    active: this.state.activeTab === "1",
+                  })}
+                  onClick={() => {
+                    this.toggle("1");
+                  }}
+                >
+                  <i className="fa fa-search" /> Busqueda simple
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({
+                    active: this.state.activeTab === "2",
+                  })}
+                  onClick={() => {
+                    this.toggle("2");
+                  }}
+                >
+                  {" "}
+                  <i className="fa fa-search-plus" /> Busqueda avanzada
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <TabContent activeTab={this.state.activeTab}>
+              <TabPane tabId="1">
+                <Row>
+                  <Col sm="10">
+                    <div>Probando</div>
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tabId="2">
+                <Row>
+                  <Col md="12">
+                    <div> Probnado</div>
+                  </Col>
+                </Row>
+              </TabPane>
+            </TabContent>
+          </div>
+          <div className="col-md-5">Paginacion de la tabla</div>
         </div>
         <br />
-        <div className="row">
-          <div
-            className="col-md-12"
-            style={{ border: "1px solid black" }}
-          ></div>
+        <div className="">
+          <div className="table">
+            <table className="table table-hover table-sm table-condensed">
+              <thead>
+                <tr
+                  className="text-center"
+                  style={{ background: "#45B254 !important" }}
+                >
+                  <th style={{ width: "10px" }}>
+                    <input
+                      type="checkbox"
+                      onClick={() =>
+                        this.setState(
+                          {
+                            checkall: !this.state.checkall,
+                          },
+                          () =>
+                            this.toggleCheckboxes(this.state.checkall, "foo")
+                        )
+                      }
+                    />
+                  </th>
+                  <th style={{ width: "150px" }}>Sede</th>
+                  <th style={{ width: "10px" }}>No.Radicación</th>
+                  <th style={{ width: "150px" }}>Asunto</th>
+                  <th style={{ width: "100px" }}>Fecha de radicación</th>
+                  <th style={{ width: "50px" }}>Destinatarios</th>
+                  <th style={{ width: "10px" }}>Estado</th>
+                  <th style={{ width: "100px" }}>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </Fragment>
     );
