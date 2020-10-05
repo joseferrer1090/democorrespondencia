b572cdeda053442e0dc9c21a98b2a087c8a8c700
cristian <cristianhz1109@gmail.com>
2020-10-05 12:31:09 -0500

    Cambios => Se ajusta la relación entre tpo documental y el asunto de la radicación

--- src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/EditCorrespondence/Components/FieldIssue.js
+++ src/views/Correspondence/ExternalCorrespondence/Inbox/Components/Client/Content/components/EditCorrespondence/Components/FieldIssue.js
@@ -1,14 +1,13 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useSelector } from "react-redux";
 const FieldIssue = ({
   field,
   form: { errors, touched, setFieldValue, setFieldTouched, values },
   ...props
 }) => {
-  const [valueInput, setValueInput] = useState("");
-
   const issueValue = useSelector(
-    (state) => state.step1ReducerInfoTypeDocumentary.infoAdditional.issue
+    (state) =>
+      state.editCorrespondenceExternalTypeDocumentary.dataTypeDocumentary.issue
   );
 
   useEffect(() => {
@@ -18,13 +17,7 @@ const FieldIssue = ({
   const validateValues = () => {
     if (issueValue !== null) {
       values.correspondence_issue = issueValue;
-      setTimeout(() => {
-        setValueInput(values.correspondence_issue);
-      }, 100);
-    } else {
-      setValueInput(values.correspondence_issue);
     }
-    return valueInput;
   };
 
   return (
@@ -32,7 +25,6 @@ const FieldIssue = ({
       <textarea
         onChange={(e) => {
           setFieldValue("correspondence_issue", e.target.value);
-          setValueInput(e.target.value);
         }}
         onBlur={(e) => setFieldTouched("correspondence_issue", true)}
         className={`form-control form-control-sm ${
@@ -41,7 +33,6 @@ const FieldIssue = ({
           "is-invalid"
         }`}
         value={values.correspondence_issue}
-        // value={valueInput}
       />
     </Fragment>
   );
